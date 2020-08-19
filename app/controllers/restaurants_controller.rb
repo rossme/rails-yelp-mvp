6f543aa5d4ac8a8c66b0bb52ac2372547ca20548
Ross Buddie <r.buddie@gmail.com>
2020-08-19 16:39:31 +0100

    Something to commit

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,32 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def edit
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+  end
+
+  def update
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,37 @@
+class ReviewsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def edit
+  end
+
+  def create
+    @review = Review.new(review_params)
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
