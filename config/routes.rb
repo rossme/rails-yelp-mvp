2ed40ff83282f506c9567e5695b3720f5a2e471f
Ross Buddie <r.buddie@gmail.com>
2020-10-17 12:26:46 +0100

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # get 'restaurants', to: 'restaurants#index'
-
-  # get 'restaurants/new', to: 'restaurants#new'
-  # get 'restaurants/:id', to: 'restaurants#show', as: :task
-
-  # post 'restaurants', to: 'restaurants#create'
-
-  # get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_task
-  # patch 'restaurants/:id', to: 'restaurants#update'
-
-  # delete 'restaurants/:id', to: 'restaurants#destroy'
-
-  resources :restaurants do
-    resources :reviews, only: [ :new, :create ]
-  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-puts "Cleaning database..."
-Restaurant.destroy_all
-
-puts "Creating restaurants..."
-dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: '03929456721', category: 'chinese' }
-best_burger = { name: "Best Burger", address: "100 Surrey High St, Lancaster E1 6PQ", phone_number: '06473729239', category: 'italian' }
-honest_china = { name: "Honest", address: "12 High Road, Kilburn", phone_number: '03284816721', category: 'chinese' }
-pizza_west = { name: "Pizza West", address: "6 Shoreham St, London E2 4TH", phone_number: '03929123351', category: 'italian' }
-dishoom_park = { name: "Dishoom Park", address: "7 Walk Away, London E4 7TR", phone_number: '03283728387', category: 'italian' }
-
-[dishoom, best_burger, honest_china, pizza_west, dishoom_park].each do |attributes|
-  restaurant = Restaurant.create!(attributes)
-  puts "Created #{restaurant.name}"
-end
-puts "Finished!"
